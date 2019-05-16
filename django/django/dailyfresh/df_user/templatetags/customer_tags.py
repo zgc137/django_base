3a3c0b8577001d16aeeda92a1b9579d3288396e1
jackwindos <935650354@qq.com>
2019-05-17 01:54:12 +0800

    simple_tag and inclusion_tag

---
+++ django/django/dailyfresh/df_user/templatetags/customer_tags.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#@author: jack
+#@contact: 935650354@qq.com
+#@site: https://www.cnblogs.com/jackzz
+#@software: PyCharm
+#@time: 2019/5/16 0016 下午 23:34
+from datetime import datetime
+from django import template
+register = template.Library()
+
+
+@register.simple_tag(name='current', takes_context=True)#
+def current_time(context):
+    return datetime.now().strftime(context['format_str'])\
+
+
+
+
+@register.inclusion_tag('df_user/show_list.html')
+def show_list(value, style):
+    return {'ls': value, 'style': style}
+
+
+
+# def current_time(context):
+#     return datetime.now().strftime(context['format_str'])
